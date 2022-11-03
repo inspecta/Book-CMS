2785359dc724ebadfeb062fbd45a4895bcd4af73
Derrick <djmulinde@gmail.com>
2022-11-03 22:39:53 +0300

    Add categoies component

---
+++ src/Components/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+const Categories = () => (
+  <>
+    <Header />
+    <div className="book-categories">
+      <h2>Categories coming soon!</h2>
+    </div>
+  </>
+);
+
+export default Categories;
