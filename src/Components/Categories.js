3889199c4604e839a1fe34d35cd964b64fea3ea4
Derrick <djmulinde@gmail.com>
2022-11-03 22:57:24 +0300

    Update Categories component

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Button from './Shared/Button';
 
 const Categories = () => (
   <>
     <Header />
     <div className="book-categories">
-      <h2>Categories coming soon!</h2>
+      <h2>Categories!</h2>
+      <Button btnName="category-btn" btnValue="Check Status" />
     </div>
   </>
 );
