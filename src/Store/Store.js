01488e6ff8c90fbd791e1f7dc23e28a42551a04e
Derrick <djmulinde@gmail.com>
2022-11-04 18:38:04 +0300

    Add static books data

---
+++ src/Store/Store.js
@@ -0,0 +1,17 @@
+const Store = [
+  {
+    id: 1,
+    title: 'The Hunger Games',
+    author: '',
+  }, {
+    id: 2,
+    title: 'Animal Farm',
+    author: 'Derrick',
+  }, {
+    id: 3,
+    title: 'War and Peace',
+    author: 'Microverse',
+  },
+];
+
+export default Store;
