6a6c433bf929473f8d0e6c27794100cea25136e5
Derrick <djmulinde@gmail.com>
2022-11-08 16:40:22 +0300

    Add redux provider component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 import App from './Components/App';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 );
-
-reportWebVitals();
