05ac6b451aefcfd246825d76d33d5efe4a03720d
Derrick <djmulinde@gmail.com>
2022-11-08 18:07:41 +0300

    Update action types  declarations

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,4 +1,4 @@
-const CHECK_BOOK_STATUS = 'CHECK_BOOK_STATUS';
+import types from '../types/types';
 
 const initialState = {
   books: [],
@@ -6,12 +6,12 @@ const initialState = {
 
 // Action creator
 export const checkBookStatus = () => ({
-  type: CHECK_BOOK_STATUS,
+  type: types.CHECK_BOOK_STATUS,
 });
 
 // Categories reducer
 const categoriesReducer = (state = initialState, action) => {
-  if (action.type === CHECK_BOOK_STATUS) {
+  if (action.type === types.CHECK_BOOK_STATUS) {
     return 'Under construction';
   }
   return state;
