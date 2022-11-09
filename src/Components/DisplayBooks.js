0670827a695d19296f37d5de7d0ceb439d677513
Derrick <djmulinde@gmail.com>
2022-11-09 11:28:58 +0300

    Add remove functionality

--- src/Components/DisplayBooks.js
+++ src/Components/DisplayBooks.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Store from '../Store/Store';
+import { useSelector } from 'react-redux';
 import Books from './Shared/Books';
 
-const DisplayBooks = () => (
-  Store.map((book) => (
-    <Books key={book.id} title={book.title} author={book.author} />
-  ))
-);
+const DisplayBooks = () => {
+  const selectedBooks = useSelector((state) => state.books);
+  return (
+    selectedBooks.books.map((book) => (
+      <Books key={book.id} title={book.title} author={book.author} id={book.id} />
+    ))
+  );
+};
 
 export default DisplayBooks;
