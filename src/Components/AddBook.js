a4cd39599b97b8859959c33ae446a09633aaedd5
Derrick <djmulinde@gmail.com>
2022-11-09 11:27:38 +0300

    Add a book using redux dispatch

--- src/Components/AddBook.js
+++ src/Components/AddBook.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBookToStore } from '../redux/books/books';
 import Button from './Shared/Button';
 
-const AddBook = () => (
-  <div className="add-book">
-    <form>
-      <input
-        type="text"
-        name="title"
-        placeholder="Enter Book Title"
-        required
-      />
-      <input
-        type="text"
-        name="author"
-        placeholder="Enter Author"
-        required
-      />
-      <Button btnName="add-book" btnValue="Add Book" />
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+
+  const [state, setState] = useState({
+    id: '',
+    title: '',
+    author: '',
+  });
+
+  const onChange = (e) => {
+    setState({
+      ...state,
+      id: uuidv4(),
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const addNewBook = (e) => {
+    e.preventDefault();
+    dispatch(addBookToStore(state));
+    setState({
+      id: '',
+      title: '',
+      author: '',
+    });
+  };
+
+  return (
+    <div className="add-book">
+      <form onSubmit={addNewBook}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Book Title"
+          onChange={onChange}
+          required
+        />
+        <input
+          type="text"
+          name="author"
+          placeholder="Enter Author"
+          onChange={onChange}
+          required
+        />
+        <Button btnName="add-book" btnValue="Add Book" />
+      </form>
+    </div>
+  );
+};
 
 export default AddBook;
