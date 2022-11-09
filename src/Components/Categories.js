29e723c3e604b8448f0a4d96a271762cb4159713
Derrick <djmulinde@gmail.com>
2022-11-09 11:28:29 +0300

    Add dispatch redux method

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkBookStatus } from '../redux/categories/categories';
 import Header from './Header';
-import Button from './Shared/Button';
 
-const Categories = () => (
-  <>
-    <Header />
-    <div className="book-categories">
-      <h2>Categories!</h2>
-      <Button btnName="category-btn" btnValue="Check Status" />
-    </div>
-  </>
-);
+const Categories = () => {
+  const selectedCategory = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const handleCheckStatus = () => {
+    dispatch(checkBookStatus());
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="book-categories">
+        <h2>Categories!</h2>
+        <h3>{selectedCategory.categories[0]}</h3>
+        <button type="submit" className="catergory-btn" onClick={() => handleCheckStatus()}>Check Status</button>
+      </div>
+    </>
+  );
+};
 
 export default Categories;
