688c8d38e0d249295e0422005183ce4a429f0aa2
Derrick <djmulinde@gmail.com>
2022-11-09 11:35:03 +0300

    Display books from the configured store

--- src/Components/DisplayBooks.js
+++ src/Components/DisplayBooks.js
@@ -6,7 +6,7 @@ const DisplayBooks = () => {
   const selectedBooks = useSelector((state) => state.books);
   return (
     selectedBooks.books.map((book) => (
-      <Books key={book.id} title={book.title} author={book.author} id={book.id} />
+      <Books key={book.id} id={book.id} title={book.title} author={book.author} />
     ))
   );
 };
