77b3a2160415d2e0c6f38f52739182dc74271312
Derrick <djmulinde@gmail.com>
2022-11-10 14:23:59 +0300

    Update with addBook slice function

--- src/Components/AddBook.js
+++ src/Components/AddBook.js
@@ -1,29 +1,31 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBookToStore } from '../redux/books/books';
+import { AddBooks } from '../redux/features/BooksSlice';
 import Button from './Shared/Button';
 
 const AddBook = () => {
   const dispatch = useDispatch();
 
   const [state, setState] = useState({
-    id: '',
+    item_id: '',
     title: '',
     author: '',
+    category: '',
   });
 
   const onChange = (e) => {
     setState({
       ...state,
-      id: uuidv4(),
+      item_id: uuidv4(),
       [e.target.name]: e.target.value,
+      category: 'Action Packed',
     });
   };
 
   const addNewBook = (e) => {
     e.preventDefault();
-    dispatch(addBookToStore(state));
+    dispatch(AddBooks(state));
     setState({
       id: '',
       title: '',
@@ -38,6 +40,7 @@ const AddBook = () => {
           type="text"
           name="title"
           placeholder="Enter Book Title"
+          value={state.title}
           onChange={onChange}
           required
         />
@@ -45,6 +48,7 @@ const AddBook = () => {
           type="text"
           name="author"
           placeholder="Enter Author"
+          value={state.author}
           onChange={onChange}
           required
         />
