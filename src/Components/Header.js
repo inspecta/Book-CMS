7d5f5ef549e4bfe4f6819f3679b7502c7da95725
Derrick <djmulinde@gmail.com>
2022-11-11 02:12:57 +0300

    Add more class names for css styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,23 +2,29 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaUser } from 'react-icons/fa';
 
-const Header = () => (
-  <header className="header flex">
-    <div className="nav flex">
-      <h1>Bookstore CMS</h1>
-      <ul className="navlinks flex">
-        <li>
-          <NavLink to="/">BOOKS</NavLink>
-        </li>
-        <li>
-          <NavLink to="/categories">CATEGORIES</NavLink>
-        </li>
-      </ul>
-    </div>
-    <div className="profile-pic flex">
-      <FaUser />
-    </div>
-  </header>
-);
+const Header = () => {
+  const activePage = ({ isActive }) => ({
+    color: isActive ? 'black' : 'grey',
+  });
+
+  return (
+    <header className="header flex white">
+      <div className="nav flex white">
+        <h1 className="color-1 white">Bookstore CMS</h1>
+        <ul className="navlinks flex">
+          <li>
+            <NavLink to="/" end style={activePage}>BOOKS</NavLink>
+          </li>
+          <li>
+            <NavLink to="/categories" style={activePage}>CATEGORIES</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="profile-pic flex">
+        <FaUser />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
